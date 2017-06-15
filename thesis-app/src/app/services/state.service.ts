377379d611de87461328f77b9b45599b36d56be7
Drewclam <andrew.c.lam94@gmail.com>
2017-06-15 14:42:43 -0400

    change state clear to empty object

--- thesis-app/src/app/services/state.service.ts
+++ thesis-app/src/app/services/state.service.ts
@@ -108,7 +108,7 @@ export class StateService implements OnDestroy {
     clearInterval(this.fetchDueBookingsIntervalId);
     clearInterval(this.fetchConfirmedBookingsIntervalId);
     clearInterval(this.fetchPendingBookingsIntervalId);
-    customerProfile = null;
+    customerProfile = {};
   }
 
 }
